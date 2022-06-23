cdf869eb433cb4f237b223c5516676525275658b
Sam Easterby-Smith <me@samscam.co.uk>
2022-06-23 10:39:46 +0100

    Initial commit - empty xcode project and gitignore

---
+++ MotorCars/MotorCarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotorCarsApp.swift
+//  MotorCars
+//
+//  Created by Sam Easterby-Smith on 23/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MotorCarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
